389af8d15ee2cf733d771c8697c01a7206da38fb
Martin Thuo <mertoenjosh3527@gmail.com>
2022-09-27 12:18:14 +0300

    > added some JS functionalities

--- script.js
+++ script.js
@@ -6,6 +6,60 @@ const btnNew = document.querySelector('.btn-add-new');
 const btnAdd = document.querySelector('.btn-add-add');
 const addDisp = document.querySelector('.add-display');
 
+const grossSalary = document.querySelector('#gross');
+const nssfContribution = document.querySelector('#nssf');
+const nhifContribution = document.querySelector('#nhif');
+const taxableIncome = document.querySelector('#taxable');
+const beforeRelief = document.querySelector('#tax-br');
+const reliefInput = document.querySelector('#relief');
+const payeInput = document.querySelector('#paye');
+const netSalary = document.querySelector('#net');
+const calculateTax = document.querySelector('.calculate1');
+
+const netPay = document.querySelector('.net2');
+const billsEls = document.querySelectorAll('.bill');
+const balancePlate = document.querySelector('.balance-plate');
+const balanceEl = document.querySelector('.bal');
+const calculateBill = document.querySelector('.calculate2');
+
+const billCategories = document.querySelector('.bill-categories')
+
+const grossTax = () => {
+  let gross = Number(grossSalary.value);
+
+  let nssf = gross * 0.06;
+  let nhif = 300;
+  let paye = gross * 0.1;
+  let relief = paye * 0.15;
+  let netPay = gross - nhif - nssf - (paye - relief);
+
+  beforeRelief.value = paye;
+  taxableIncome.value = gross;
+  nhifContribution.value = nhif;
+  nssfContribution.value = nssf;
+  payeInput.value = paye;
+  reliefInput.value = relief;
+  netSalary.value = netPay;
+  console.log(gross);
+};
+
+const bill = () => {
+  let available = Number(netPay.value);
+  let bills = [...billsEls].map(it => Number(it.value));
+  let balance = available;
+  
+  let totalBill = bills.reduce((acc, el) =>  (acc + el), 0);
+  console.log(totalBill)
+
+  if (totalBill > available) {
+  } else {
+    balance = available - totalBill;
+  }
+
+  balancePlate.classList.remove('hidden');
+  balanceEl.textContent = balance;
+};
+
 const taxManager = () => {
   btnTaxManager.classList.add('active');
   btnTaxManager.classList.remove('inactive');
@@ -30,6 +84,7 @@ const addBill = () => {
   addDisp.classList.add('hidden');
   btnNew.classList.remove('hidden');
 };
+
 const newBill = () => {
   btnNew.classList.add('hidden');
   addDisp.classList.remove('hidden');
@@ -39,3 +94,6 @@ btnTaxManager.addEventListener('click', taxManager);
 btnBillManager.addEventListener('click', billManager);
 btnAdd.addEventListener('click', addBill);
 btnNew.addEventListener('click', newBill);
+
+calculateTax.addEventListener('click', grossTax);
+calculateBill.addEventListener('click', bill);
